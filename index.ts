f69fe3c4d544706b3f79c12981e66145ade89909
ohexus <moskalenko.taras.d@gmail.com>
2021-01-13 13:53:32 +0200

    added responseTimeMiddleware and log after bot start

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import { Context } from 'telegraf';
 
 import bot from './src/bot';
+
 import CommandController from './src/controllers/command.controller';
-import { startMarkdown, helpMarkdown } from './src/markdowns';
+
+import responseTimeMiddleware from './src/middlewares/responseTime.middleware';
 
 // bot.start((ctx: Context) => ctx.reply(startMarkdown, { parse_mode: 'MarkdownV2' }));
 // bot.help((ctx: Context) => ctx.reply(helpMarkdown, { parse_mode: 'MarkdownV2' }));
 
+bot.use(responseTimeMiddleware);
+
 bot.start((ctx: Context) => CommandController.onStart(ctx));
 bot.help((ctx: Context) => CommandController.onHelp(ctx));
 
-bot.launch();
+// Start bot
+bot.launch().then(() => console.log('bot started successfully.'));
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
