9e5f45b3198f1c068a540f3929fa893be33deae0
ohexus <moskalenko.taras.d@gmail.com>
2021-01-13 13:52:19 +0200

    replaced reply by replyWithMarkdownV2

--- src/controllers/command.controller.ts
+++ src/controllers/command.controller.ts
@@ -4,16 +4,12 @@ import { helpMarkdown, startMarkdown } from '../markdowns';
 class CommandController {
   constructor() {}
 
-  markdownReply(ctx: Context, markdown: string) {
-    ctx.reply(markdown, { parse_mode: 'MarkdownV2' });
-  }
-
   onStart(ctx: Context) {
-    this.markdownReply(ctx, startMarkdown);
+    ctx.replyWithMarkdownV2(startMarkdown);
   }
 
   onHelp(ctx: Context) {
-    this.markdownReply(ctx, helpMarkdown);
+    ctx.replyWithMarkdownV2(helpMarkdown);
   }
 }
 
