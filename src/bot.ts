d5d319a3618a321253a5c42aea449f5c64f82c71
ohexus <moskalenko.taras.d@gmail.com>
2021-01-14 12:57:15 +0200

    moved bot settings to bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,24 @@
 import config from 'config';
+import { Context, Middleware, Telegraf } from 'telegraf';
 
-import { Telegraf } from 'telegraf';
+import logMessageMiddleware from './middlewares/logMessage.middleware';
+import responseTimeMiddleware from './middlewares/responseTime.middleware';
 
+import commandRouter from './routes/command.routes';
+
+// Initialize bot
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || config.get('TELEGRAM_BOT_TOKEN'));
+
+// Set webhook
+bot.telegram.setWebhook(process.env.WEB_HOOK_URI || config.get('WEB_HOOK_URI'));
+
+// Log message
+bot.use(logMessageMiddleware as Middleware<Context>);
+
+// Log response time
+bot.use(responseTimeMiddleware);
+
+// Hear command
+bot.hears(/\/.[^ ]+/g, commandRouter);
 
 export default bot;
