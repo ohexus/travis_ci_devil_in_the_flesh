af96fcdfe033e7dae809d22559771d3c8d035f8f
ohexus <moskalenko.taras.d@gmail.com>
2021-01-14 13:02:20 +0200

    added express server

--- index.ts
+++ index.ts
@@ -1,18 +1,29 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import config from 'config';
+
+import appRouter from './src/routes/app.routes';
+
 import bot from './src/bot';
 
-import responseTimeMiddleware from './src/middlewares/responseTime.middleware';
+const app: Application = express();
 
-import commandRouter from './src/routes/command.routes';
+app.use(cors());
 
-// Log response time
-bot.use(responseTimeMiddleware);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Hear command
-bot.hears(/\/.[^ ]+/g, commandRouter);
+app.use('/', appRouter);
 
 // Start bot
-bot.launch().then(() => console.log('bot started successfully.'));
+bot.launch().then(() => console.log('Bot started successfully.'));
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+// Start server
+const PORT = process.env.PORT || config.get('PORT');
+app.listen(PORT, () => {
+  console.log('Listening on port %s', PORT);
+});
