b1c671070254ac2b377fbc6fc118a75f6045c778
ohexus <moskalenko.taras.d@gmail.com>
2021-01-15 03:19:50 +0200

    added log4js and connectDB

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import config from 'config';
+import log4js from 'log4js';
 
 import appRouter from './src/routes/app.routes';
+
+import { connectDB } from './src/utils/db';
+
+import { LOGS } from './src/constants';
 
 import bot from './src/bot';
 
 const app: Application = express();
+
+const logger = log4js.getLogger();
+logger.level = process.env.LOGGER_LVL || config.get('LOGGER_LVL');
+
+const dbConnection = connectDB();
+if (!dbConnection) {
+  throw new Error(LOGS.ERROR.DB.CONNECTION);
+}
 
 app.use(cors());
 
