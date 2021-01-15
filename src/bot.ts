7e55347817c28d0afcf1454dddaec50d5c90373b
ohexus <moskalenko.taras.d@gmail.com>
2021-01-15 03:19:08 +0200

    added BotContext interface, added middlewares: session, logMessageMiddleware, added messageRouter

--- src/bot.ts
+++ src/bot.ts
@@ -1,24 +1,32 @@
 import config from 'config';
-import { Context, Middleware, Telegraf } from 'telegraf';
+import { session, Telegraf } from 'telegraf';
 
 import logMessageMiddleware from './middlewares/logMessage.middleware';
 import responseTimeMiddleware from './middlewares/responseTime.middleware';
 
-import commandRouter from './routes/command.routes';
+import commandRouter from './routes/bot/command.routes';
+import messageRouter from './routes/bot/message.routes';
+
+import { commandRegexp } from './regexps';
+
+import BotContext from './interfaces/BotContext';
 
 // Initialize bot
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || config.get('TELEGRAM_BOT_TOKEN'));
+const bot = new Telegraf<BotContext>(process.env.TELEGRAM_BOT_TOKEN || config.get('TELEGRAM_BOT_TOKEN'));
 
-// Set webhook
-bot.telegram.setWebhook(process.env.WEB_HOOK_URI || config.get('WEB_HOOK_URI'));
+// Use session
+bot.use(session());
 
 // Log message
-bot.use(logMessageMiddleware as Middleware<Context>);
+bot.use(logMessageMiddleware);
 
 // Log response time
 bot.use(responseTimeMiddleware);
 
 // Hear command
-bot.hears(/\/.[^ ]+/g, commandRouter);
+bot.hears(commandRegexp, commandRouter);
+
+// Hear message
+bot.on('text', messageRouter);
 
 export default bot;
