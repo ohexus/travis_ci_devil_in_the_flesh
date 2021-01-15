335bef3daa8151259bf56b6754acf2557e8bf8a4
ohexus <moskalenko.taras.d@gmail.com>
2021-01-15 19:02:43 +0200

    added INFO level by default

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,6 @@ import config from 'config';
 import log4js from 'log4js';
 
 const logger = log4js.getLogger();
-logger.level = process.env.LOGGER_LVL || config.get('LOGGER_LVL');
+logger.level = process.env.LOGGER_LVL || config.get('LOGGER_LVL') || 'INFO';
 
 export default logger;
