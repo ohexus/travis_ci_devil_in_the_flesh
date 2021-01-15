86e944a84cee8d5cd106e66d6fcf08786c0cc8c9
ohexus <moskalenko.taras.d@gmail.com>
2021-01-15 18:13:47 +0200

    added build notification

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -2,18 +2,35 @@ import { Request, Response } from 'express';
 
 import bot from '../bot';
 
+import logger from '../utils/logger';
+
+import { UserService } from '../services';
+import repoLinkService from '../services/repoLink.service';
+
 import buildStatusHTML from '../markups/buildStatusHTML';
+
+import TravisPayload from '../interfaces/TravisPayload';
 
 class NotificationsController {
   constructor() {}
 
   // TODO
-  async postNotify(req: Request, res: Response) {
-    console.log(req.body);
+  async postNotify(req: Request, res: Response): Promise<void> {
+    try {
+      const { payload }: { payload: TravisPayload } = req.body;
 
-    const buildHTML = buildStatusHTML(req.body);
+      const repoLink = await repoLinkService.getLinkByUrl(payload.repository.url);
 
-    bot.telegram.sendMessage(0, buildHTML, { parse_mode: 'HTML' });
+      if (!!repoLink) {
+        const user = await UserService.getUserById(repoLink.owner);
+
+        const buildHTML = buildStatusHTML(payload);
+
+        bot.telegram.sendMessage(user.chatId, buildHTML, { parse_mode: 'HTML' });
+      }
+    } catch (err) {
+      logger.error(err);
+    }
   }
 }
 
