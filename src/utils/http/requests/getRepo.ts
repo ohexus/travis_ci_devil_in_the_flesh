7cc2937030b15c5c08b92757f9f46166ef565f79
ohexus <moskalenko.taras.d@gmail.com>
2021-01-16 16:16:12 +0200

    added error handler

--- src/utils/http/requests/getRepo.ts
+++ src/utils/http/requests/getRepo.ts
@@ -1,10 +1,16 @@
 import { githubAPI } from '../API';
 
 import GithubRepo from '../../../interfaces/entities/GithubRepo';
+import logger from '../../logger';
 
-export default async function getRepo(owner: string, repo: string): Promise<GithubRepo> {
-  return await githubAPI.get(`repos/${owner}/${repo}`).then((res) => {
-    const { id, name, full_name, html_url } = res.data;
-    return { id, name, full_name, html_url } as GithubRepo;
-  });
+export default async function getRepo(owner: string, repo: string): Promise<GithubRepo | null> {
+  try {
+    return await githubAPI.get(`repos/${owner}/${repo}`).then((res) => {
+      const { id, name, full_name, html_url } = res.data;
+      return { id, name, full_name, html_url } as GithubRepo;
+    });
+  } catch (err) {
+    logger.error(err);
+    return null;
+  }
 }
