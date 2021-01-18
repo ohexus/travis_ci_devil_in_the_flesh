3e9ed0beceed2fc822acbe36586f2d0ed4ebdda6
ohexus <moskalenko.taras.d@gmail.com>
2021-01-18 14:18:05 +0200

    added server.close() after process stop

--- index.ts
+++ index.ts
@@ -27,12 +27,18 @@ app.use('/', appRouter);
 // Start bot
 bot.launch().then(() => console.log('Bot started successfully.'));
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
 // Start server
 const PORT = process.env.PORT || config.get('PORT');
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Listening on port %s', PORT);
+});
+
+// Enable graceful stop
+process.once('SIGINT', () => {
+  bot.stop('SIGINT');
+  server.close();
+});
+process.once('SIGTERM', () => {
+  bot.stop('SIGTERM');
+  server.close();
 });
