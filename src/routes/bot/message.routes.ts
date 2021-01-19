cb8a8422463828127f26dab058fb509509114c10
ohexus <moskalenko.taras.d@gmail.com>
2021-01-19 13:46:01 +0200

    removed unnecessary checks

--- src/routes/bot/message.routes.ts
+++ src/routes/bot/message.routes.ts
@@ -1,4 +1,3 @@
-import c from 'config';
 import CommandController from '../../controllers/command.controller';
 
 import Steps from '../../enums/Steps';
@@ -6,19 +5,17 @@ import Steps from '../../enums/Steps';
 import BotContext from '../../interfaces/BotContext';
 
 export default function messageRouter(ctx: BotContext) {
-  if (!!ctx.message && !!ctx.message.text && !!ctx.session) {
-    switch (ctx.session.step) {
-      case Steps.LINK:
-        CommandController.onLinkReply(ctx);
-        break;
+  switch (ctx.session.step) {
+    case Steps.LINK:
+      CommandController.onLinkReply(ctx);
+      break;
 
-      case Steps.DELETE:
-        CommandController.onDeleteReply(ctx);
-        break;
+    case Steps.DELETE:
+      CommandController.onDeleteReply(ctx);
+      break;
 
-      default:
-        CommandController.onHelp(ctx);
-        break;
-    }
+    default:
+      CommandController.onHelp(ctx);
+      break;
   }
 }
