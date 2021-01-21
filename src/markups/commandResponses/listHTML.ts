79ae3a6082bfd641898f767aa204d6ecac2d3ec0
ohexus <moskalenko.taras.d@gmail.com>
2021-01-21 13:05:20 +0200

    renamed withLinks argument to showLinks

--- src/markups/commandResponses/listHTML.ts
+++ src/markups/commandResponses/listHTML.ts
@@ -2,11 +2,11 @@ import { RepoDoc } from '../../interfaces/entities/Repo';
 
 export default function listHTML(
   repos: RepoDoc[],
-  withLinks: boolean = true,
+  showLinks: boolean = true,
   heading: string = 'repos',
   afterword?: string,
 ): string {
   return `${heading}:\n\n${repos
-    .map((repoDoc, index) => `${index + 1}. ${repoDoc.repo.name}${withLinks ? `:\n${repoDoc.repo.html_url}` : ''};`)
+    .map((repoDoc, index) => `${index + 1}. ${repoDoc.repo.name}${showLinks ? `:\n${repoDoc.repo.html_url}` : ''};`)
     .reduce((acc: string, curr: string) => acc + curr + '\n\n', '')}${afterword || ''}`;
 }
