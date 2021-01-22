4d56a1850a8eb0d1e1de66f65a59672131d38c1d
ohexus <moskalenko.taras.d@gmail.com>
2021-01-22 16:34:51 +0200

    renamed Repo to RepoBasic, added to Repo interface field "secret"

--- src/interfaces/entities/Repo.ts
+++ src/interfaces/entities/Repo.ts
@@ -3,10 +3,14 @@ import { Document } from 'mongoose';
 import { ChatDoc } from './Chat';
 import GithubRepo from './GithubRepo';
 
-export interface Repo {
+export interface RepoBasic {
   owner: ChatDoc['id'];
   title: string;
   repo: GithubRepo;
+}
+
+export interface Repo extends RepoBasic {
+  secret: string;
 }
 
 export interface RepoDoc extends Repo, Document {}
